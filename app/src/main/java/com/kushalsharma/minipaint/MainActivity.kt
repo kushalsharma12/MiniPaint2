2f47dc1986b6a593865bb07e71b98161423fd1d3
kushalsharma12 <uie19bme1081@gmail.com>
2021-08-19 14:49:26 +0530

    toast added

--- app/src/main/java/com/kushalsharma/minipaint/MainActivity.kt
+++ app/src/main/java/com/kushalsharma/minipaint/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kushalsharma.minipaint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(myCanvasView)
 
         println("Something useful is going on, working to change the world and know why I am living. Life is fragile and one can die at any time or soon")
+
+        Toast.makeText(this, "Start drawing with clarity.", Toast.LENGTH_SHORT).show()
     }
 }
