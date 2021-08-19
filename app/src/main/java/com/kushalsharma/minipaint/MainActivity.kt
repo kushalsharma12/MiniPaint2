a1404a6c12ca15af6f21e27795de7504203e14da
kushalsharma12 <uie19bme1081@gmail.com>
2021-08-19 15:24:02 +0530

    Revert "bad commit"
    This reverts commit 04bdef7b

--- app/src/main/java/com/kushalsharma/minipaint/MainActivity.kt
+++ app/src/main/java/com/kushalsharma/minipaint/MainActivity.kt
@@ -3,7 +3,6 @@ package com.kushalsharma.minipaint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
         println("Something useful is going on, working to change the world and know why I am living. Life is fragile and one can die at any time or soon")
 
-        Toast.makeText(this, "Start drawing with clarity.", Toast.LENGTH_SHORT).show()
-        lsjkfalsdjfa
+        we gotta go now
     }
 }
